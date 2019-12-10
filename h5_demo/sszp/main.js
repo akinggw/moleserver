342d8a90edb9067a81bb06a7fec9a2761929f827
Ou <ou@ubuntu.ubuntu-domain>
2019-12-10 15:12:21 +0800

    游戏启动全屏

--- h5_demo/sszp/main.js
+++ h5_demo/sszp/main.js
@@ -2,11 +2,11 @@ cc.game.onStart = function(){
    // if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
    //     document.body.removeChild(document.getElementById("cocosLoading"));
 
-    var designSize = cc.size(480,800);
+    var designSize = cc.size(800,480);
     var screenSize = cc.view.getFrameSize();
 
     if(!cc.sys.isNative && screenSize.height < 800){
-        designSize = cc.size(320,480);
+        designSize = cc.size(480,320);
         cc.loader.resPath = "res";
     }else{
         cc.loader.resPath = "res";
